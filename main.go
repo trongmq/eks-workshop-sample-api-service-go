61d84ec6056f92aa7a11aa1598336422b761b851
Trọng Mạch <97945341+trongmq@users.noreply.github.com>
2023-03-14 15:45:22 +0700

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "trọng quá là đẹp trai"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
